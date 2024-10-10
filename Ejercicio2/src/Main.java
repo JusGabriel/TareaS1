f8ba2ad986a6d5731df8dd74334900b842b6efe9
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2024-10-09 20:30:43 -0500

    Update Main.java

--- Ejercicio2/src/Main.java
+++ Ejercicio2/src/Main.java
@@ -2,27 +2,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Libro libro1 = new Libro();
-        libro1.titulo = "Huasipungo";
-        libro1.autor = "Jorge Icaza";
-        libro1.paginaCount = 182;
+        Libro libro1 =new Libro();
+        libro1.titulo ="Huasipungo";
+        libro1.autor ="Jorge Icaza";
+        libro1.paginaCount= 182;
 
-        Libro libro2 = new Libro();
-        libro2.titulo = "Cumandá";
-        libro2.autor = "Juan León Mera";
-        libro2.paginaCount = 244;
+        Libro libro2= new Libro();
+        libro2.titulo ="Cumandá";
+        libro2.autor ="Juan León Mera";
+        libro2.paginaCount= 244;
 
         System.out.println(libro1.infoLibro());
         System.out.println(libro2.infoLibro());
     }
-}
+    }
 
 class Libro {
     String titulo;
     String autor;
     int paginaCount;
 
-    String infoLibro() {
+    String infoLibro(){
         return "Título: " + titulo + ", Autor: " + autor + ", Páginas: " + paginaCount;
     }
 }
